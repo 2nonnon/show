2205f35fb04c66eb6fcb4cdc17183fe00b3ca550
2nonnon <lanpangzi0205@gmail.com>
2021-10-21 14:43:17 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:2nonnon/a-blog.git main:gh-pages
+# git push -u git@github.com:2nonnon/a-blog.git main:gh-pages
+git push -u https://github.com/2nonnon/2nonnon.GitHub.io.git master:main
 
 cd -
