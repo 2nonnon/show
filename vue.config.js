9709ed5754e23de5c5ec4f59384ab3e1a1afac6a
2nonnon <lanpangzi0205@gmail.com>
2021-10-21 13:49:38 +0800

    something

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,6 @@ module.exports = {
             args[0].title = '2nonnon'      
             return args
         }) 
-    } 
+    },
+    publicPath: process.env.NODE_ENV === "production" ? "/a-blog/" : "/"
 }
